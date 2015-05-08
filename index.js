9ac7149c57fce200fa4286363ed0aa49e54a9dd8
Marc Farra <marcfarra@gmail.com>
2015-05-08 14:15:39 -0400

    Fix style

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2FtaWN1dCIsImEiOiJMVzF2NThZIn0.WO0ArcIIzYVioen3HpfugQ';
 
-var map = L.mapbox.map('map', 'examples.map-h68a1pf7').setView([10,10],5);
+var map = L.mapbox.map('map', 'mapbox.light').setView([10,10],5);
 var polys = {
   'type': 'FeatureCollection',
   'features': []
@@ -18,29 +18,31 @@ for (var i=0; i<50; i++) {
   var y = Math.random() * 10;
   var extent = [x,y,x+width,y+height];
   var bbox = turf.bboxPolygon(extent);
+  bbox.style = {
+    fillOpacity: 0.08,
+    weight: 0,
+    color: 'black'
+  };
   bboxes.push(bbox);
   turf.pointGrid(extent,0.5,'degrees').features.forEach(function(feature) {
     points.features.push(feature);
   });
 }
-console.log(points);
 
 polys.features = bboxes;
 
 var extent = turf.extent(polys);
-var grid = turf.hexGrid(extent, 1, 'degrees');
+var grid = turf.squareGrid(extent, 1, 'degrees');
 
 grid = turf.count(grid, points, 'pt_count');
 grid.features.forEach(function (feature) {
-  var style = feature.style = {};
+  var style = feature.style = {
+    color: 'black',
+    weight:0,
+  };
   var pt_count = feature.properties.pt_count;
-  style.color = '#2020C9';
   style.weight = 0;
-  style.fill = '#9A8FBF';
   style.fillOpacity = 0;
-  //if(pt_count >= 0) {
-  //style.fillOpacity = 0.00;
-  //}
   if(pt_count >= 1) {
     style.fillOpacity = 0.2;
     style.weight = 1;
@@ -58,7 +60,9 @@ var griddedLayer = L.geoJson(grid).eachLayer(function (layer) {
     layer.setStyle(layer.feature.style);
   });
 
-var extentLayer = L.geoJson(polys);
+var extentLayer = L.geoJson(polys).eachLayer(function(layer) {
+  layer.setStyle(layer.feature.style);
+});
 
 gridBtn = document.getElementById('grid-btn');
 extentBtn = document.getElementById('extent-btn');
@@ -68,11 +72,11 @@ extentBtn.addEventListener('click', attachLayer(extentBtn, extentLayer));
 
 function colorButton(btn) {
   btn.style['background-color'] = '#2ecc71';
-  btn.style['color'] = 'white';
+  btn.style.color = 'white';
 }
 function deColorButton(btn) {
   btn.style['background-color'] = 'white';
-  btn.style['color'] = 'black';
+  btn.style.color = 'black';
 }
 colorButton(extentBtn);
 extentLayer.addTo(map);
